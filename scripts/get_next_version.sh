97a3843eb7cf3a4674e4d512fe23fba88354da8b
Rey Abolofia <purple4reina@gmail.com>
2025-09-14 21:19:17 -0700

    Trim extra git tag postfix.

--- scripts/get_next_version.sh
+++ scripts/get_next_version.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-CURRENT_VERSION="$(git describe --tags || echo "v1.0.0")"
+LAST_TAG="$(git describe --tags || echo "v1.0.0")"
+CURRENT_VERSION="$(echo "$LAST_TAG" | tr '-' ' ' | awk '{print $1}')"
 echo "Old version: $CURRENT_VERSION"
+
 export VITE_PRACTICE_VERSION="$(echo $CURRENT_VERSION | awk -F. -v OFS=. '{$(NF-1) += 1 ; print}')"
 echo "New version: $VITE_PRACTICE_VERSION"
