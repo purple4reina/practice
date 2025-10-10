d146f0a0f6e4421c498954d20fd308f9859e7f72
Rey Abolofia <purple4reina@gmail.com>
2025-10-09 20:41:54 -0400

    Add left/right control pains to block elements.

--- src/blocks/block.ts
+++ src/blocks/block.ts
@@ -26,12 +26,54 @@ export abstract class Block implements IBlock {
 
   protected newBlockDiv(parent: HTMLElement): HTMLElement {
     const type = (<typeof Block> this.constructor).type;
-    const div = document.createElement('div');
-    parent.appendChild(div);
-    div.classList.add("row");
-    div.classList.add("block-element");
-    div.classList.add(type);
-    div.setAttribute('id', this.id);
-    return div;
+
+    const envelope = document.createElement("div");
+    parent.appendChild(envelope);
+    envelope.classList.add("container");
+    envelope.classList.add("row");
+    envelope.classList.add("block-element");
+
+    // left controls
+    const leftControls = document.createElement("div");
+    envelope.appendChild(leftControls);
+    leftControls.classList.add("col-1");
+    leftControls.classList.add("left-controls");
+
+    // block
+    const block = document.createElement("div");
+    envelope.appendChild(block);
+    block.classList.add("col");
+
+    const blockBody = document.createElement("div");
+    block.appendChild(blockBody);
+    blockBody.classList.add("row");
+    blockBody.classList.add("block-body");
+    blockBody.classList.add(type);
+    blockBody.setAttribute("id", this.id);
+
+    // right controls
+    const rightControls = document.createElement("div");
+    envelope.appendChild(rightControls);
+    rightControls.classList.add("col-1");
+    rightControls.classList.add("right-controls");
+
+    const trash = document.createElement("i");
+    rightControls.appendChild(trash);
+    trash.classList.add("bi");
+    trash.classList.add("bi-trash");
+    trash.classList.add("bi-tiny");
+    trash.classList.add("block-control");
+    trash.style.display = "inline-block";
+
+    // hovers
+    const overables = envelope.getElementsByClassName("block-control");
+    envelope.addEventListener("mouseenter", () => {
+      [...overables].forEach(e => (e as HTMLElement).style.color = "red");
+    });
+    envelope.addEventListener("mouseleave", () => {
+      [...overables].forEach(e => (e as HTMLElement).style.color = "white");
+    });
+
+    return block;
   }
 }
