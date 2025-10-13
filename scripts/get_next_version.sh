629156ed19deab78ab536687f050cf4111b994ee
Rey Abolofia <purple4reina@gmail.com>
2025-10-12 19:52:42 -0700

    Fix version dots.

--- scripts/get_next_version.sh
+++ scripts/get_next_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-LAST_TAG="$(git tag | tr '.' ' ' | tr 'v' ' ' | sort -k 1 -k 2 -h | tr ' ' '.' | tail -n 1)"
-CURRENT_VERSION=v"$(echo "$LAST_TAG" | tr '-' ' ' | awk '{print $1}')"
+LAST_TAG="$(git tag | tr '.' ' ' | tr 'v' ' ' | sort -k 1 -k 2 -h | awk '{print "v"$1"."$2"."$3}' | tail -n 1)"
+CURRENT_VERSION="$(echo "$LAST_TAG" | tr '-' ' ' | awk '{print $1}')"
 echo "Old version: $CURRENT_VERSION"
 
 export VITE_PRACTICE_VERSION="$(echo $CURRENT_VERSION | awk -F. -v OFS=. '{$(NF-1) += 1 ; print}')"
