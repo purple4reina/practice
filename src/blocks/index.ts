8d31a4f21de9531d616ec5d19b73e64e73ddaf46
Rey Abolofia <purple4reina@gmail.com>
2025-11-15 21:19:24 -0800

    Add final extra beat.

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -135,6 +135,8 @@ export default class BlockManager {
         }
       }
     }
+    // yield one final click as a downbeat
+    yield { delay: 350, strong: true, recording: true };
   }
 
   getRecordDelays() {
