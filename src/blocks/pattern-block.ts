f2c5b37426c1d7bbe701c97fbe1793bb03842659
Rey Abolofia <purple4reina@gmail.com>
2025-11-27 16:11:57 -0700

    Allow starting on pickup notes.

--- src/blocks/pattern-block.ts
+++ src/blocks/pattern-block.ts
@@ -5,6 +5,7 @@ export default class PatternBlock extends Block {
   static readonly type = "pattern";
 
   private beats;
+  private start;
 
   constructor(parent: HTMLElement, opts: any) {
     super();
@@ -26,16 +27,28 @@ export default class PatternBlock extends Block {
             </div>
           </div>
         </div>
-        <div class="col"><!-- empty column --></div>
+        <div class="col">
+          <div class="container">
+            <div class="row-col">
+              Starting Beat:
+            </div>
+            <div class="row-col input-group">
+              <button class="btn" id="${this.id}-start-minus" type="button" tabindex="-1">-</button>
+              <input type="text" class="form-control" id="${this.id}-start-val" value="4" pattern="[0-9]*">
+              <button class="btn" id="${this.id}-start-plus" type="button" tabindex="-1">+</button>
+            </div>
+          </div>
+        </div>
         <div class="col"><!-- empty column --></div>
         <div class="col"><!-- empty column --></div>
       </div>
     `;
     this.beats = plusMinusControls(`${this.id}-beats`, { initial: opts.beats || 4, min: 1, max: 16 });
+    this.start = plusMinusControls(`${this.id}-start`, { initial: opts.start || 1, min: 1, max: 16 });
   }
 
   *clickIntervalGen(phase: "record" | "play", state: ClickState) {
-    state.beatIndex = 0;
+    state.beatIndex = this.start() - 1;
     state.beatsPerMeasure = this.beats();
   }
 
