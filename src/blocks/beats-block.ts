811d5cd47f92a8111108ffe12144e3315bd4cf23
Rey Abolofia <purple4reina@gmail.com>
2025-12-06 23:58:26 -0800

    Correctly get click level.

--- src/blocks/beats-block.ts
+++ src/blocks/beats-block.ts
@@ -48,7 +48,7 @@ export default class BeatsBlock extends Block {
 
     const delay = 60 / bpm / subdivisions * 1000;
     for (let i = 0; i < this.count(); i++) {
-      const level = (state.beatIndex % state.beatsPerMeasure) === 0 ? 1 : 2;
+      const level = state.getLevel();
       yield* addClick({ delay, level: level, recording });
       for (let j = 0; j < subdivisions - 1; j++) {
         yield* addClick({ delay, level: 4, recording });
