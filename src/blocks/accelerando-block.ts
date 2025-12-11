8944c18e5094be4491388fe18166382fabc3325a
Rey Abolofia <purple4reina@gmail.com>
2025-12-10 23:15:49 -0800

    Specify accelerando kind.

--- src/blocks/accelerando-block.ts
+++ src/blocks/accelerando-block.ts
@@ -2,6 +2,7 @@ import { Block, ClickState } from "./block";
 
 export default class AccelerandoBlock extends Block {
   static readonly type = "accelerando";
+  private kind;
 
   constructor(parent: HTMLElement) {
     super();
@@ -12,11 +13,22 @@ export default class AccelerandoBlock extends Block {
           <strong>Accelerando</strong>
         </div>
         <div class="col"><!-- empty column --></div>
-        <div class="col"><!-- empty column --></div>
+        <div class="col">
+          <div class="container">
+            <div class="row-col input-group">
+              <select class="form-select" id="${this.id}-accel-kind">
+                <option value="linear">Linear</option>
+                <option value="percentage" disabled>Percentage</option>
+              </select>
+            </div>
+          </div>
+        </div>
         <div class="col"><!-- empty column --></div>
         <div class="col"><!-- empty column --></div>
       </div>
     `;
+    const kindSelect = document.getElementById(`${this.id}-accel-kind`) as HTMLSelectElement;
+    this.kind = () => kindSelect.value;
   }
 
   *clickIntervalGen(phase: "record" | "play", state: ClickState) {
