8cf71da1b553f3945c95eb3c07a7ec9ee20af344
Rey Abolofia <purple4reina@gmail.com>
2025-12-16 03:08:31 -0800

    Implement parabola accel.

--- src/blocks/accel-functions.ts
+++ src/blocks/accel-functions.ts
@@ -14,7 +14,14 @@ export const accelFunctions = {
     return (-B + Math.sqrt(B**2 - 4*A*C)) / (2*A);
   },
 
+  parabola: (opts: accelFunctionOpts): number => {
+    const k = (opts.finalTempo - opts.initialTempo) / opts.totalClicks;
+    if (Math.abs(k) < 1e-15) {
+      return opts.thisClick / opts.initialTempo;
+    }
+    return Math.log(1 + (opts.thisClick * k) / opts.initialTempo) / k;
+  },
+
   // TODO:
-  // parabola
   // "square root"
 };
