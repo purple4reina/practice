1239a09a9170ed7e60d526ebb47937e429936cd7
Rey Abolofia <purple4reina@gmail.com>
2026-01-14 18:11:29 -0800

    Use compressed query params when url too long.

--- src/query-params.ts
+++ src/query-params.ts
@@ -1,15 +1,44 @@
+import {
+  compressToEncodedURIComponent,
+  decompressFromEncodedURIComponent,
+} from 'lz-string';
+
 export default class QueryParams {
+  private static compressedKey = 'c';
   private static params: URLSearchParams = new URLSearchParams(window.location.search);
 
+  private static getDecompressedParams(): URLSearchParams {
+    const compressed = this.params.get(this.compressedKey);
+    if (compressed) {
+      const decompressed = decompressFromEncodedURIComponent(compressed);
+      if (decompressed) {
+        return new URLSearchParams(decompressed);
+      }
+    }
+    return new URLSearchParams();
+  }
+
   public static get(key: string): string | null {
+    const decompressedParams = this.getDecompressedParams();
+    if (decompressedParams.has(key)) {
+      return decompressedParams.get(key);
+    }
     return this.params.get(key);
   }
 
   public static getAll(): IterableIterator<[string, string]> {
+    const decompressedParams = this.getDecompressedParams();
+    if ([...decompressedParams.keys()].length > 0) {
+      return decompressedParams.entries();
+    }
     return this.params.entries();
   }
 
   public static has(key: string): boolean {
+    const decompressedParams = this.getDecompressedParams();
+    if (decompressedParams.has(key)) {
+      return true;
+    }
     return this.params.has(key);
   }
 
@@ -32,6 +61,13 @@ export default class QueryParams {
 
   private static updateURL(): void {
     const newUrl = `${window.location.pathname}?${this.params.toString()}`;
-    window.history.replaceState({}, '', newUrl);
+    if (newUrl.length > 2000) {
+      const params = new URLSearchParams();
+      params.set(this.compressedKey, compressToEncodedURIComponent(this.params.toString()));
+      const compressedUrl = `${window.location.pathname}?${params.toString()}`;
+      window.history.replaceState({}, '', compressedUrl);
+    } else {
+      window.history.replaceState({}, '', newUrl);
+    }
   }
 }
